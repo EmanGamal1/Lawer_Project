88be9b6201a126345780d951415a3551e29634c8
Eman Gamal <emangaamal@gmail.com>
2023-06-16 20:31:39 +0200

    verify account

--- src/router/index.ts
+++ src/router/index.ts
@@ -43,7 +43,7 @@ const router = createRouter({
       path: '/user-profile',
       name: 'user-profile',
       component: () => import('../views/UserProfile/UserProfile.vue')
-    },
+    }
   ]
 })
 
