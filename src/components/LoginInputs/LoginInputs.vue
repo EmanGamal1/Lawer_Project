08886fdf02fa153792360decb16efcc9a0ec738d
Eman Gamal <emangaamal@gmail.com>
2023-06-17 03:46:46 +0200

    login validation

--- src/components/LoginInputs/LoginInputs.vue
+++ src/components/LoginInputs/LoginInputs.vue
@@ -1,52 +1,121 @@
 <template>
     <form @submit.prevent="login">
         <label for="phone">رقـم الهــاتف</label>
-        <input type="text" class="form-control" v-model="phone" name="phone" id="phone" placeholder="رقـم الهـاتف" />
-        <label for="phone_code">كود رقـم الهــاتف</label>
-        <input type="text" class="form-control" v-model="phoneCode" name="phone_code" id="phone_code" placeholder="كود رقـم الهـاتف" />
-        <label for="password">كلمــة المرور</label>
-        <input type="password" class="form-control" v-model="password" name="password" id="password" placeholder="كلمــة المرور" />
+        <div class="form-group">
+            <input type="text" class="form-control" v-model="phone" name="phone" id="phone" placeholder="رقـم الهـاتف" />
+            <span class="errors">{{ phoneErrorMessage }}</span>
+        </div>
+        <div class="form-group">
+            <label for="phone_code">كود الدولـــة</label>
+            <input type="text" class="form-control" v-model="phone_code" name="phone_code" id="phone_code" placeholder="كود رقـم الهــاتف" />
+            <span class="errors">{{ phoneCodeErrorMessage }}</span>
+        </div>
+        <div class="form-group">
+            <label for="password">كلمــة المرور</label>
+            <input type="password" class="form-control" v-model="password" name="password" id="password" placeholder="كلمــة المرور" />
+            <span class="errors">{{ passwordErrorMessage }}</span>
+        </div><br/>
         <input type="submit" class="btn allButtons form-control" value="دخــول" />
         <router-link to="/forget-password" class="routeSpan">نسيــت كلمـة المــرور؟</router-link>
     </form>
 </template>
 
 <style scoped>
-input {
-    margin-bottom: 20px;
+label {
+    margin-top: 20px;
 }
 </style>
 
 <script>
-import {axiosInstance} from '@/Axios';
+import { axiosInstance } from '@/Axios';
+import { useField, useForm } from 'vee-validate';
+import { countryCodeValidation, phoneValidation, passwordValidation } from '@/validationRules';
+import { useRouter } from 'vue-router';
 
 export default {
-    data() {
-        return {
-            phone: '',
-            phoneCode: '',
-            password: ''
-        };
+  props: {
+    phoneErrorMessage: {
+      type: String,
+      default: '',
     },
-    methods: {
-        async login() {
-            try {
-                const response = await axiosInstance.post('/api/client_web/login', {
-                    phone: this.phone,
-                    phone_code: this.phoneCode,
-                    password: this.password
-                });
-                if (response.status === 200) {
-                    const token = response.data.data.token;
-                    localStorage.setItem('token', token);
-                    this.$router.push('/');
-                } else {
-                    console.log("Failed to login");
-                }
-            } catch (error) {
-                    console.log("error");
-            }
+    phoneCodeErrorMessage: {
+      type: String,
+      default: '',
+    },
+    passwordErrorMessage: {
+      type: String,
+      default: '',
+    },
+  },
+  setup(props) {
+    const router = useRouter();
+
+    const { handleSubmit, resetForm } = useForm();
+
+    const {
+      errorMessage: phoneErrorMessage,
+      value: phone,
+      handleBlur: phoneBlur,
+    } = useField('phone', phoneValidation);
+
+    const {
+      errorMessage: phoneCodeErrorMessage,
+      value: phone_code,
+      handleBlur: phoneCodeBlur,
+    } = useField('phone_code', countryCodeValidation);
+
+    const {
+      errorMessage: passwordErrorMessage,
+      value: password,
+      handleBlur: passwordBlur,
+    } = useField('password', passwordValidation);
+
+    const login = handleSubmit(async () => {
+      try {
+        const requestData = {};
+
+        if (phone && phone.value) {
+          requestData.phone = phone.value;
         }
-    }
+
+        if (phone_code && phone_code.value) {
+          requestData.phone_code = phone_code.value;
+        }
+
+        if (password && password.value) {
+          requestData.password = password.value;
+        }
+
+        const response = await axiosInstance.post('/api/client_web/login', requestData);
+
+        if (response.status === 200) {
+          const token = response.data.data.token;
+          localStorage.setItem('token', token);
+          router.push('/');
+        } else {
+          console.log('Failed to login');
+        }
+      } catch (error) {
+        console.log('Error occurred during login:', error);
+      }
+    });
+
+    return {
+      phoneErrorMessage,
+      phone,
+      phoneBlur,
+      phoneCodeErrorMessage,
+      phone_code,
+      phoneCodeBlur,
+      passwordErrorMessage,
+      password,
+      passwordBlur,
+      login,
+      router,
+    };
+  },
+  data() {
+    return {};
+  },
 };
 </script>
